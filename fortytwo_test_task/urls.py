93f15656669f1b54b30c92b0390d8a9e63f98049
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-21 05:48:52 -0700

    ticket:1 added forgotten imports

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,6 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
