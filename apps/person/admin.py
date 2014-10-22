3a2126a16283f984f6f961448d856bf6b6e08a6a
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-22 05:14:22 -0700

    ticket:1 Person model added to the admin site

--- apps/person/admin.py
+++ apps/person/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from apps.person.models import Person
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    model = Person
+
+    def full_name(self, person):
+        return "%s %s" % (person.name, person.surname)
+
+    fieldsets = [
+        ('Core data',   {'fields': [['name', 'surname', 'birth'], 'bio']}),
+        ('Contacts',    {'fields': [['email', 'skype', 'jabber'], 'contacts']}),
+    ]
+
+    list_display = ('full_name', 'birth', 'email')
+
+
+admin.site.register(Person, PersonAdmin)
