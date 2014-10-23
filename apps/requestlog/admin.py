0f61f913a12fcadf7606ea7524ce1b549dea9821
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-23 01:07:49 -0700

    ticket:3 RequestLog model added to the admin site

--- apps/requestlog/admin.py
+++ apps/requestlog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from apps.requestlog.models import RequestLog
 
-# Register your models here.
+
+class RequestLogAdmin(admin.ModelAdmin):
+    model = RequestLog
+
+    fieldsets = [
+        ('Time and result',
+            {'fields': [['time_start', 'time_end', 'response_code']]}),
+
+        ('Data',
+            {'fields': [['path', 'query', 'method'], 'address']}),
+    ]
+
+    list_display = ('time_start', 'path', 'method', 'response_code', 'address')
+
+
+admin.site.register(RequestLog, RequestLogAdmin)
