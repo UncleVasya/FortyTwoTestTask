97d871b7ad17a9ed612e369100f46b2c5dde4117
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-23 01:53:53 -0700

    ticket:3 dummy view with empty template

--- apps/requestlog/views.py
+++ apps/requestlog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from apps.requestlog.models import RequestLog
 
-# Create your views here.
+
+class RequestsView(generic.ListView):
+
+    def get_queryset(self):
+        return RequestLog.objects.all().select_related()
