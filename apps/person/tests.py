b6e15acf0d7f3cc097202e92961bf490b3fe7bc0
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-24 02:04:51 -0700

    ticket:5 test to check person update with valid data

--- apps/person/tests.py
+++ apps/person/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils.datetime_safe import date
 from apps.person.models import Person
 
 
@@ -91,3 +92,39 @@ class IndexViewTests(TestCase):
         self.assertTemplateUsed(resp, 'person/person_detail.html')
         self.assertTrue(self.is_person_shown(person, resp.content))
         self.assertFalse(self.is_person_shown(self.person1, resp.content))
+
+
+class UpdateViewTests(TestCase):
+    UPDATE_URL = reverse('person:person_update')
+    SUCCESS_REDIRECT_URL = reverse('person:index')
+
+    def setUp(self):
+        self.new_data = {
+            'name':     'Jane',
+            'surname':  'Doe',
+            'birth':    date(1980, 7, 2),
+            'bio':      'I am a person for test',
+            'email':    'jane.doe@gmail.com',
+            'jabber':   'jane.doe@jabber.com',
+            'skype':    'jane_doe',
+            'contacts': 'Nope'
+        }
+
+    def test_update_with_valid_data(self):
+        """
+            Person in DB should be updated with new values.
+
+            User should be redirected to the index page
+        """
+        resp = self.client.post(self.UPDATE_URL, self.new_data)
+
+        person = Person.objects.first()
+        self.assertEqual(person.name,       self.new_data['name'])
+        self.assertEqual(person.surname,    self.new_data['surname'])
+        self.assertEqual(person.birth,      self.new_data['birth'])
+        self.assertEqual(person.email,      self.new_data['email'])
+        self.assertEqual(person.jabber,     self.new_data['jabber'])
+        self.assertEqual(person.skype,      self.new_data['skype'])
+        self.assertEqual(person.contacts,   self.new_data['contacts'])
+
+        self.assertRedirects(resp, self.SUCCESS_REDIRECT_URL)
