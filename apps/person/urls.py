23ad644aa0a1a332d3b719745aa8d4a244ea66ba
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-24 02:50:18 -0700

    ticket:5 person update page access restricted to anonymous users

--- apps/person/urls.py
+++ apps/person/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from apps.person import views
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^update/$', views.PersonUpdateView.as_view(), name='person_update'),
+    url(r'^update/$', login_required(views.PersonUpdateView.as_view()),
+        name='person_update'),
 )
