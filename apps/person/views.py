705e64af971c79df39523cccf924a33dd82f3f53
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-24 02:06:48 -0700

    ticket:5 person update implementation

--- apps/person/views.py
+++ apps/person/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse_lazy
 from django.http import Http404
 from django.views import generic
 from apps.person.models import Person
@@ -11,5 +12,11 @@ class IndexView(generic.DetailView):
         return person
 
 
-class PersonUpdateView(generic.View):
-    pass
+class PersonUpdateView(generic.UpdateView):
+    success_url = reverse_lazy('person:index')
+
+    def get_object(self):
+        person = Person.objects.first()
+        if not person:
+            raise Http404
+        return person
