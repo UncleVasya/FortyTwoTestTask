453d4f9eada4de3a1d838e7588f3faad598c7d55
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-10-27 09:45:40 -0700

    ticket:5 some tests refactored; template mess fixed after previous commits

--- apps/person/tests.py
+++ apps/person/tests.py
@@ -7,31 +7,25 @@ import apps
 from apps.person.models import Person
 
 
+def is_person_shown(person, content):
+    """
+        Helper function to check if rendered html
+        contains data about specific person
+    """
+    fields = [
+        person.name,
+        person.surname,
+        person.email,
+        person.jabber,
+        person.skype,
+        person.birth.strftime('%Y-%m-%d')
+    ] + person.bio.split('\r\n') + \
+        person.contacts.split('\r\n')
+
+    return all(entry in content for entry in fields)
+
+
 class IndexViewTests(TestCase):
-
-    def is_person_shown(self, person, content):
-        """
-            Helper function to check if rendered html
-            contains data about specific person
-        """
-        result = True
-        result = result and (person.name in content)
-        result = result and (person.surname in content)
-        result = result and (person.email in content)
-        result = result and (person.jabber in content)
-        result = result and (person.skype in content)
-
-        birth_date = person.birth.strftime('%b. %d, %Y')
-        result = result and (birth_date in content)
-
-        for entry in person.bio.split('\r\n'):
-            result = result and (entry in content)
-
-        for entry in person.contacts.split('\r\n'):
-            result = result and (entry in content)
-
-        return result
-
     def setUp(self):
         super(IndexViewTests, self).setUp()
         self.person1 = Person.objects.all()[0]
@@ -70,8 +64,8 @@ class IndexViewTests(TestCase):
 
         # check html render
         self.assertTemplateUsed(resp, 'person/person_detail.html')
-        self.assertTrue(self.is_person_shown(person, resp.content))
-        self.assertFalse(self.is_person_shown(self.person2, resp.content))
+        self.assertTrue(is_person_shown(person, resp.content))
+        self.assertFalse(is_person_shown(self.person2, resp.content))
 
     def test_index_success_after_removal(self):
         """
@@ -93,8 +87,8 @@ class IndexViewTests(TestCase):
 
         # check html render
         self.assertTemplateUsed(resp, 'person/person_detail.html')
-        self.assertTrue(self.is_person_shown(person, resp.content))
-        self.assertFalse(self.is_person_shown(self.person1, resp.content))
+        self.assertTrue(is_person_shown(person, resp.content))
+        self.assertFalse(is_person_shown(self.person1, resp.content))
 
 
 class UpdateViewTests(TestCase):
@@ -104,13 +98,13 @@ class UpdateViewTests(TestCase):
 
     def setUp(self):
         self.new_data = {
-            'name':     'Jane',
-            'surname':  'Doe',
-            'birth':    date(1980, 7, 2),
-            'bio':      'I am a person for test',
-            'email':    'jane.doe@gmail.com',
-            'jabber':   'jane.doe@jabber.com',
-            'skype':    'jane_doe',
+            'name': 'Jane',
+            'surname': 'Doe',
+            'birth': date(1980, 7, 2),
+            'bio': 'I am a person for test',
+            'email': 'jane.doe@gmail.com',
+            'jabber': 'jane.doe@jabber.com',
+            'skype': 'jane_doe',
             'contacts': 'Nope'
         }
 
@@ -119,8 +113,17 @@ class UpdateViewTests(TestCase):
             password='admin'
         )
 
+        self.person1 = Person.objects.all()[0]
+        self.person2 = Person.objects.all()[1]
+
     def tearDown(self):
         self.client.logout()
+
+    def test_get_update_page(self):
+        resp = self.client.get(self.UPDATE_URL)
+
+        self.assertTrue(is_person_shown(self.person1, resp.content))
+        self.assertFalse(is_person_shown(self.person2, resp.content))
 
     def test_update_with_valid_data(self):
         """
@@ -131,13 +134,13 @@ class UpdateViewTests(TestCase):
         resp = self.client.post(self.UPDATE_URL, self.new_data)
 
         person = Person.objects.first()
-        self.assertEqual(person.name,       self.new_data['name'])
-        self.assertEqual(person.surname,    self.new_data['surname'])
-        self.assertEqual(person.birth,      self.new_data['birth'])
-        self.assertEqual(person.email,      self.new_data['email'])
-        self.assertEqual(person.jabber,     self.new_data['jabber'])
-        self.assertEqual(person.skype,      self.new_data['skype'])
-        self.assertEqual(person.contacts,   self.new_data['contacts'])
+        self.assertEqual(person.name, self.new_data['name'])
+        self.assertEqual(person.surname, self.new_data['surname'])
+        self.assertEqual(person.birth, self.new_data['birth'])
+        self.assertEqual(person.email, self.new_data['email'])
+        self.assertEqual(person.jabber, self.new_data['jabber'])
+        self.assertEqual(person.skype, self.new_data['skype'])
+        self.assertEqual(person.contacts, self.new_data['contacts'])
 
         self.assertRedirects(resp, self.SUCCESS_REDIRECT_URL)
 
--- selenium_tests/test_person.py
+++ selenium_tests/test_person.py
@@ -20,20 +20,17 @@ class PersonIndexPageTest(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
 
         person = Person.objects.first()
+        fields = [
+            person.name,
+            person.surname,
+            person.email,
+            person.jabber,
+            person.skype,
+            person.birth.strftime('%Y-%m-%d')
+        ] + person.bio.split('\r\n') + \
+            person.contacts.split('\r\n')
 
-        self.assertIn(person.name, body.text)
-        self.assertIn(person.surname, body.text)
-        self.assertIn(person.email, body.text)
-        self.assertIn(person.jabber, body.text)
-        self.assertIn(person.skype, body.text)
-
-        birth_date = person.birth.strftime('%b. %d, %Y')
-        self.assertIn(birth_date, body.text)
-
-        for entry in person.bio.split('\r\n'):
-            self.assertIn(entry, body.text)
-
-        for entry in person.contacts.split('\r\n'):
+        for entry in fields:
             self.assertIn(entry, body.text)
 
     def test_only_one_person_shown(self):
@@ -44,20 +41,16 @@ class PersonIndexPageTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element_by_tag_name('body')
 
-        name_labels = body.find_elements_by_id('label_name')
-        surname_labels = body.find_elements_by_id('label_surname')
-        birth_labels = body.find_elements_by_id('label_birth')
-        bio_labels = body.find_elements_by_id('label_bio')
-        email_labels = body.find_elements_by_id('label_email')
-        jabber_labels = body.find_elements_by_id('label_jabber')
-        skype_labels = body.find_elements_by_id('label_skype')
-        contacts_labels = body.find_elements_by_id('label_contacts')
+        labels = [
+            'label_name',
+            'label_surname',
+            'label_birth',
+            'label_bio',
+            'label_email',
+            'label_jabber',
+            'label_skype',
+            'label_contacts',
+        ]
 
-        self.assertEqual(1, len(name_labels))
-        self.assertEqual(1, len(surname_labels))
-        self.assertEqual(1, len(birth_labels))
-        self.assertEqual(1, len(bio_labels))
-        self.assertEqual(1, len(email_labels))
-        self.assertEqual(1, len(jabber_labels))
-        self.assertEqual(1, len(skype_labels))
-        self.assertEqual(1, len(contacts_labels))
+        for label in labels:
+            self.assertEqual(1, len(body.find_elements_by_id(label)))
