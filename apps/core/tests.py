1aa4b36180b5bb6e97322a4b0096d7e156723acf
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-11-04 12:55:41 +0200

    ticket:8 added test for template tag

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,6 +1,9 @@
 from django.conf import settings
-from django.template import RequestContext
+from django.core import urlresolvers
+from django.template import RequestContext, Template, Context
 from django.test import TestCase, RequestFactory
+from apps.person.models import Person
+from apps.requestlog.models import RequestLog
 
 
 class SettingsAdderProcessorTests(TestCase):
@@ -16,3 +19,34 @@ class SettingsAdderProcessorTests(TestCase):
 
         self.assertTrue('settings' in ctx)
         self.assertEqual(ctx['settings'], settings)
+
+
+class EditTagTests(TestCase):
+
+    def test_tag_with_valid_input(self):
+        """
+            Edit tag should be rendered into
+            admin site link for provided object
+        """
+        person = Person.objects.first()
+        request = RequestLog.objects.first()
+
+        html = Template(
+            "{% load edit_link %}"
+            "{% edit_link person %}"
+            "{% edit_link request %}"
+        ).render(Context({
+            'person': person,
+            'request': request
+        }))
+
+        self.assertIn(
+            urlresolvers.reverse('admin:person_person_change',
+                                 args=(person.id,)),
+            html
+        )
+        self.assertIn(
+            urlresolvers.reverse('admin:requestlog_requestlog_change',
+                                 args=(request.id,)),
+            html
+        )
