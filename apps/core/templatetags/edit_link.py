32522e0482b63a8d51e9bc4e5fe84a2245412c9f
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-11-05 15:40:02 +0200

    ticket:8 made use of simple_tag

--- apps/core/templatetags/edit_link.py
+++ apps/core/templatetags/edit_link.py
@@ -4,22 +4,9 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 
-@register.tag
-def edit_link(parser, token):
-    try:
-        tag_name, obj_name = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    return EditLinkNode(obj_name)
+@register.simple_tag
+def edit_link(obj):
+    app, model = obj._meta.app_label, obj._meta.module_name
 
-
-class EditLinkNode(template.Node):
-    def __init__(self, obj_name):
-        self.obj = template.Variable(obj_name)
-
-    def render(self, context):
-        obj = self.obj.resolve(context)
-        app, model = obj._meta.app_label, obj._meta.module_name
-
-        return '<a href=%s>admin</a>' % \
-               reverse('admin:%s_%s_change' % (app, model), args=[obj.id])
+    return '<a href=%s>admin</a>' % \
+           reverse('admin:%s_%s_change' % (app, model), args=[obj.id])
