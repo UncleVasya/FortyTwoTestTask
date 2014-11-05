5c548c6b5a12f658aa1144312ca0d7a577bd3a15
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-11-05 15:15:11 +0200

    ticket:9 command output to stdout/stderr

--- apps/core/management/commands/print_models.py
+++ apps/core/management/commands/print_models.py
@@ -6,7 +6,11 @@ class Command(NoArgsCommand):
     help = "Prints all project models and the count of objects in every model"
 
     def handle_noargs(self, **options):
+        self.stderr.write('Error:')
         for ct in ContentType.objects.all():
             m = ct.model_class()
-            print "%s.%s\t%d" % \
-                  (m.__module__, m.__name__, m.objects.all().count())
+            to_print = "%s.%s\t%d" % \
+                       (m.__module__, m.__name__, m.objects.all().count())
+
+            self.stdout.write(to_print)
+            self.stderr.write(to_print)
