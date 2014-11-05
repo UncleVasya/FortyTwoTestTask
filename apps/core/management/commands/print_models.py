0c495ed312d51c0f486997628ff554e254437263
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-11-05 13:29:23 +0200

    ticket:9 command implementation

--- apps/core/management/commands/print_models.py
+++ apps/core/management/commands/print_models.py
@@ -1,6 +1,12 @@
-from django.core.management.base import BaseCommand
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import NoArgsCommand
 
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = "Prints all project models and the count of objects in every model"
 
+    def handle_noargs(self, **options):
+        for ct in ContentType.objects.all():
+            m = ct.model_class()
+            print "%s.%s\t%d" % \
+                  (m.__module__, m.__name__, m.objects.all().count())
