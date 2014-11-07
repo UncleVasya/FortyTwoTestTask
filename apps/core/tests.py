80f46a1427513329115218f67d186c88b2f97de1
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2014-11-07 22:38:32 +0200

    ticket:10 added tests

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, Template, Context
 from django.test import TestCase, RequestFactory
+from django.utils.timezone import now
+from apps.core.models import OperationLog
 from apps.person.models import Person
 from apps.requestlog.models import RequestLog
 
@@ -21,30 +23,95 @@ class SettingsAdderProcessorTests(TestCase):
         self.assertEqual(ctx['settings'], settings)
 
 
-class EditTagTests(TestCase):
+class OperationLogTest(TestCase):
 
-    def test_tag_with_valid_input(self):
+    def test_object_created(self):
         """
-            Edit tag should be rendered into
-            admin site link for provided object
+            After object created, DB should contain
+            corresponding OperationLog entry.
+
+            Data in the log entry should be correct.
         """
-        person = Person.objects.first()
+        log_count = OperationLog.objects.count()
+
+        request = RequestLog.objects.create(
+            path='some/path',
+            method='GET',
+            time_start=now(),
+            time_end=now(),
+            response_code=404,
+        )
+        log_entry = OperationLog.objects.last()
+
+        # check if new log entry added
+        self.assertEqual(OperationLog.objects.count(),
+                         log_count + 1)
+
+        # check if log entry is correct
+        self.assertEqual(log_entry.operation, 'created')
+        self.assertEqual(log_entry.obj_class, RequestLog.__name__)
+        self.assertEqual(log_entry.obj_module, RequestLog.__module__)
+        self.assertEqual(log_entry.obj_pk, str(request.pk))
+
+    def test_object_updated(self):
+        """
+            After object updated, DB should contain
+            corresponding OperationLog entry.
+
+            Data in the log entry should be correct.
+        """
+        log_count = OperationLog.objects.count()
+
+        # update some logged request
         request = RequestLog.objects.first()
+        request.response = 777
+        request.save()
 
-        html = Template(
-            "{% load edit_link %}"
-            "{% edit_link person %}"
-            "{% edit_link request %}"
-        ).render(Context({
-            'person': person,
-            'request': request
-        }))
+        log_entry = OperationLog.objects.last()
 
-        self.assertIn(
-            reverse('admin:person_person_change', args=(person.id,)),
-            html
-        )
-        self.assertIn(
-            reverse('admin:requestlog_requestlog_change', args=(request.id,)),
-            html
-        )
+        # check if new log entry added
+        self.assertEqual(OperationLog.objects.count(),
+                         log_count + 1)
+
+        # check if log entry is correct
+        self.assertEqual(log_entry.operation, 'updated')
+        self.assertEqual(log_entry.obj_class, RequestLog.__name__)
+        self.assertEqual(log_entry.obj_module, RequestLog.__module__)
+        self.assertEqual(log_entry.obj_pk, str(request.pk))
+
+    def test_object_deleted(self):
+        """
+            After object deleted, DB should contain
+            corresponding OperationLog entry.
+
+            Data in the log entry should be correct.
+        """
+        log_count = OperationLog.objects.count()
+
+        # delete some logged request
+        request = RequestLog.objects.first()
+        request_pk = request.pk
+        request.delete()
+
+        log_entry = OperationLog.objects.last()
+
+        # check if new log entry added
+        self.assertEqual(OperationLog.objects.count(),
+                         log_count + 1)
+
+        # check if log entry is correct
+        self.assertEqual(log_entry.operation, 'deleted')
+        self.assertEqual(log_entry.obj_class, RequestLog.__name__)
+        self.assertEqual(log_entry.obj_module, RequestLog.__module__)
+        self.assertEqual(log_entry.obj_pk, str(request_pk))
+
+    def test_log_objects_ignored(self):
+        """
+            Changes of OperationLog objects
+            should NOT be logged in DB.
+        """
+        log_count = OperationLog.objects.count()
+
+        OperationLog.objects.first().operation = 'blabla'
+
+        self.assertEqual(OperationLog.objects.count(), log_count)
